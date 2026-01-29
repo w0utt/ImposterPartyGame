38a7e05ecc2ea93f5caa12c1d788ca67015a11c6
Wout Brugmans <57450200+w0utt@users.noreply.github.com>
2026-01-29 11:30:10 +0100

    add categories to config

--- config.js
+++ config.js
@@ -5,28 +5,264 @@ export const MODES = {
 
 export const GAME_CONFIG = {
   categories: [
-    {
-      naam: "Beroemde voetballer",
-      woorden: [
-        "Lionel Messi","Cristiano Ronaldo","Kevin De Bruyne","Eden Hazard",
-        "Romelu Lukaku","Thibaut Courtois","Kylian Mbappé","Neymar",
-        "Erling Haaland","Robert Lewandowski","Harry Kane","Luka Modrić",
-        "Karim Benzema","Mohamed Salah","Virgil van Dijk","Sadio Mané",
-        "Antoine Griezmann","Jude Bellingham","Vinícius Júnior","Manuel Neuer",
-        "Ronaldinho","Zinedine Zidane","Andrés Iniesta"
-      ]
-    },
-    {
-      naam: "Bekende Vlaming (BV)",
-      woorden: [
-        "Stromae","Koen Wauters","Niels Destadsbader","Gert Verhulst",
-        "Marthe De Pillecyn","James Cooke","Dimitri Vegas","K3",
-        "Willy Sommers","Erik Van Looy","Tom Waes","Jani Kazaltzis",
-        "Andy Peelman","Philippe Geubels"
-      ]
-    }
-    // 👉 rest categorieën exact zoals origineel
-  ],
+        {
+          naam: "Beroemde voetballer",
+          woorden: [
+            "Lionel Messi",
+            "Cristiano Ronaldo",
+            "Kevin De Bruyne",
+            "Eden Hazard",
+            "Romelu Lukaku",
+            "Thibaut Courtois",
+            "Kylian Mbappé",
+            "Neymar",
+            "Erling Haaland",
+            "Robert Lewandowski",
+            "Harry Kane",
+            "Luka Modrić",
+            "Karim Benzema",
+            "Mohamed Salah",
+            "Virgil van Dijk",
+            "Sadio Mané",
+            "Antoine Griezmann",
+            "Jude Bellingham",
+            "Vinícius Júnior",
+            "Manuel Neuer",
+            "Ronaldinho",
+            "Zinedine Zidane",
+            "Andrés Iniesta"
+          ]
+        },
+        {
+          naam: "Bekende Vlaming (BV)",
+          woorden: [
+            "Stromae",
+            "Koen Wauters",
+            "Niels Destadsbader",
+            "Gert Verhulst",
+            "Marthe De Pillecyn",
+            "James Cooke",
+            "Dimitri Vegas",
+            "K3",
+            "Willy Sommers",
+            "Erik Van Looy",
+            "Tom Waes",
+            "Jani Kazaltzis",
+            "Andy Peelman",
+            "Philippe Geubels"
+          ]
+        },
+        {
+          naam: "Internationale zanger of zangeres",
+          woorden: [
+            "Taylor Swift",
+            "Ed Sheeran",
+            "Beyoncé",
+            "Adele",
+            "The Weeknd",
+            "Dua Lipa",
+            "Billie Eilish",
+            "Harry Styles",
+            "Kanye West",
+            "Bruno Mars",
+            "Lady Gaga",
+            "Rihanna",
+            "Justin Bieber",
+            "Sam Smith",
+            "Ariana Grande",
+            "Shakira",
+            "Selena Gomez",
+            "Olivia Rodrigo",
+          ]
+        },
+        {
+          naam: "Internationale acteur of actrice",
+          woorden: [
+            "Leonardo DiCaprio",
+            "Tom Cruise",
+            "Scarlett Johansson",
+            "Ryan Reynolds",
+            "Jennifer Lawrence",
+            "Dwayne Johnson",
+            "Zendaya",
+            "Keanu Reeves"
+          ]
+        },
+        {
+          naam: "Bekende politicus",
+          woorden: [
+            "Alexander De Croo",
+            "Bart De Wever",
+            "Emmanuel Macron",
+            "Angela Merkel",
+            "Donald Trump",
+            "Barack Obama",
+            "Ursula von der Leyen",
+            "Greta Thunberg",
+            "Tom Van Grieken",
+            "Joe Biden"
+          ]
+        },
+        {
+          naam: "Bekende YouTuber / creator",
+          woorden: [
+            "MrBeast",
+            "PewDiePie",
+            "Enzo Knol",
+            "Kastiop",
+            "Acid",
+            "KSI",
+            "Logan Paul",
+            "Jake Paul"
+          ]
+        },
+        {
+          naam: "Land",
+          woorden: [
+            "België",
+            "Nederland",
+            "Frankrijk",
+            "Duitsland",
+            "Spanje",
+            "Portugal",
+            "Italië",
+            "Zweden",
+            "Noorwegen",
+            "Finland",
+            "Japan",
+            "China",
+            "Brazilië",
+            "Argentinië",
+            "Australië",
+            "Canada",
+            "Mexico",
+            "Marokko",
+            "Egypte",
+            "India"
+          ]
+        },
+        {
+          naam: "Grote stad",
+          woorden: [
+            "Brussel",
+            "Antwerpen",
+            "Gent",
+            "Amsterdam",
+            "Rotterdam",
+            "Parijs",
+            "Londen",
+            "Berlijn",
+            "Madrid",
+            "Barcelona",
+            "New York",
+            "Los Angeles",
+            "Tokio",
+            "Osaka",
+            "Dubai",
+            "Singapore",
+            "Sydney",
+            "Rio de Janeiro",
+            "Kaapstad",
+            "Istanbul"
+          ]
+        },
+        {
+          naam: "Bekende sporter (niet-voetbal)",
+          woorden: [
+            "Lewis Hamilton",
+            "Max Verstappen",
+            "Novak Djokovic",
+            "Roger Federer",
+            "Serena Williams",
+            "Tiger Woods",
+            "Michael Phelps",
+            "Usain Bolt",
+            "Conor McGregor"
+          ]
+        },
+        {
+          naam: "Bekend merk of bedrijf",
+          woorden: [
+            "Apple",
+            "Google",
+            "Tesla",
+            "Mercedes-Benz",
+            "BMW",
+            "Coca-Cola",
+            "McDonald's",
+            "Nike",
+            "Adidas",
+            "Samsung",
+            "Sony",
+            "Red Bull"
+          ]
+        },
+        {
+          naam: "Fictief personage",
+          woorden: [
+            "Harry Potter",
+            "Lord Voldemort",
+            "Batman",
+            "Joker",
+            "Spider-Man",
+            "Darth Vader",
+            "Yoda",
+            "Elsa",
+            "Shrek",
+            "Goku",
+            "Iron Man",
+            "Captain America",
+            "Thor",
+            "Hulk",
+            "Hermione Granger",
+            "Severus Snape",
+            "Luke Skywalker",
+            "SpongeBob SquarePants",
+            "Homer Simpson",
+            "Katniss Everdeen"
+          ]
+        },
+        {
+          naam: "Eten",
+          woorden: [
+            "Pizza",
+            "Sushi",
+            "Hamburger",
+            "Frieten",
+            "Lasagne",
+            "Tacos",
+            "Döner",
+            "Loempia",
+            "Pita",
+            "Ravioli",
+            "Croque monsieur",
+            "Spaghetti",
+            "Bami",
+            "Curry",
+            "Paella",
+            "Falafel",
+            "Burrito",
+            "Ramen",
+            "Kebab",
+            "Wrap",
+            "Chili con carne",
+            "Focaccia",
+            "Hotdog",
+            "Quiche",
+            "Baklava",
+            "Gyoza",
+            "Kip saté",
+            "Moussaka",
+            "Risotto",
+            "Bruschetta",
+            "Pancakes",
+            "Waffles",
+            "Tiramisu",
+            "Macaroni",
+            "Nachos",
+          ]
+        }
+      ],
 
   prompts: [
     { publiek: "Wat vind jij het ergste insect?", imposter: "Welk insect zou je zijn als je er één kon zijn?" },
